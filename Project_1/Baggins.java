e336797d322147fbeae5386d720c98b4352a014a
Taylor Roberts <taylorproberts@u.boisestate.edu>
2019-09-27 14:15:22 -0600

    Added Grocery class in Baggins.java

--- Project_1/Baggins.java
+++ Project_1/Baggins.java
@@ -1,3 +1,35 @@
-public static void main(string, args[]){
-    
+
+
+
+public Baggins (){
+
+
+
+
+
+
+   
+}
+
+
+pulic class groceries(){
+
+    vector<sack> = sacks;
+    ArrayList<Item> unpackedItems;
+
+    public Solution(Solution s)
+    {
+        sacks = new Vector<Bag>();
+        for (Sack i: s.sacks) sacks.add(new Sack(i));
+        upackedItems = new ArrayList<Item>(s.unpackedItems);
+    }
+
+    public Solution(){
+        unpackedItems = new ArrayList<Item>(items.values());
+        sacks = new Vector<Bag>(numBags);
+        for (int x = 0; x < numSacks; x++) sacks.add(new Bag(0));
+    }
+
+}
+
 }
